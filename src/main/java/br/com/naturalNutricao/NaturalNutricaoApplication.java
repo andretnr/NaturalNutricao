81048f689113ff43ace97c21ced5f04c96f2fe35
andretnr <andretnr@gmail.com>
2021-12-01 02:22:31 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/br/com/naturalNutricao/NaturalNutricaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.naturalNutricao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalNutricaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalNutricaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/naturalnutricao/NaturalnutricaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.naturalnutricao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalnutricaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

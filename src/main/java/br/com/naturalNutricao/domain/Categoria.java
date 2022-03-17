2d22356c53a41671299b9923607b75423e77b7a4
andretnr <andretnr@gmail.com>
2022-03-17 17:05:24 -0300

    Testando a primeira classe de dominio

---
+++ src/main/java/br/com/naturalNutricao/domain/Categoria.java
@@ -0,0 +1,67 @@
+package br.com.naturalnutricao.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private int Id;
+	private String name;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(int id, String name) {
+		Id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (Id != other.Id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [Id=" + Id + ", name=" + name + "]";
+	}
+	
+	
+
+}
--- src/main/java/br/com/naturalNutricao/resources/CategoriaResource.java
+++ src/main/java/br/com/naturalNutricao/resources/CategoriaResource.java
@@ -1,12 +1,15 @@
 package br.com.naturalnutricao.resources;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.naturalnutricao.domain.Categoria;
 import ch.qos.logback.classic.Logger;
 
 @RestController
@@ -16,9 +19,17 @@ public class CategoriaResource implements Serializable{
 	public static final Logger logger = (Logger) LoggerFactory.getLogger(CategoriaResource.class);
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Nome Teste");
+		Categoria cat2 = new Categoria(2, "Nome Teste dois");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
 		logger.info("Rest OK");
-		return "Rest funcional";
+		return lista;
 		
 	}
 	
